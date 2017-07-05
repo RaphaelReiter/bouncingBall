c44c243c704a12924ff955ce5f7cdc0d5c117487
Raphaël Reiter <raphaelreiter@raphaels-imac.local>
2017-07-05 10:33:49 +0200

    Initial Commit

---
+++ StarWithDynamics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWithDynamics
+//
+//  Created by Raphaël Reiter on 05/07/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
